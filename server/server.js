ba4e2ce4ce901efa203cc7b1a950a9a2157a6130
Yofri <yofriadiyahya@gmail.com>
2017-08-12 19:24:59 +0700

    Test DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ app.delete('/todos/:id', (req, res) => {
   Todo.findByIdAndRemove(id).then((todo) => {
     if (!todo) return res.status(404).send();
 
-    res.send(todo);
+    res.send({todo});
   }).catch(() => res.status(400).send());
 });
 
