f71bf9a1fd03d77000873446c2911af5c94d45a1
Yofri <yofriadiyahya@gmail.com>
2017-08-13 08:25:42 +0700

    Extract controller to controllers

--- server/server.js
+++ server/server.js
@@ -2,83 +2,19 @@ require('./config/config');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const _ = require('lodash');
-
-const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/todo');
-const {User} = require('./models/user');
+const controller = require('./controllers/controller');
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
-  let todo = new Todo({
-    text: req.body.text
-  });
-
-  todo.save().then((doc) => {
-    res.send(doc);
-  }).catch(() => res.status(400).send());
-});
-
-app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
-    res.send({todos});
-  }).catch(() => res.status(400).send());
-});
-
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id;
-
-  if (!ObjectID.isValid(id)) return res.status(404).send();
-
-  Todo.findById(id).then((todo) => {
-    if (!todo) return res.status(404).send();
-
-    res.send({todo});
-  }).catch(() => res.status(400).send());
-});
-
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id;
-
-  if (!ObjectID.isValid(id)) return res.status(404).send();
-
-  Todo.findByIdAndRemove(id).then((todo) => {
-    if (!todo) return res.status(404).send();
-
-    res.send({todo});
-  }).catch(() => res.status(400).send());
-});
-
-app.patch('/todos/:id', (req, res) => {
-  const id = req.params.id;
-  const body = _.pick(req.body, ['text', 'completed']);
-
-  if (!ObjectID.isValid(id)) return res.status(404).send();
-
-  if (_.isBoolean(body.completed) && body.completed) {
-    body.completedAt = new Date().getTime();
-  } else {
-    body.completed = false;
-    body.completedAt = null;
-  }
-
-  Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
-    .then((todo) => {
-      if (!todo) return res.status(404).send();
-
-      res.send({todo});
-    }).catch(() => {
-      res.status(404).send();
-    });
-});
-
-app.listen(port, () => {
-  console.log(`App started on port ${port}`);
-});
+app.post('/todos', controller.postTodos);
+app.get('/todos', controller.getTodos);
+app.get('/todos/:id', controller.getTodosId);
+app.delete('/todos/:id', controller.deleteTodosId);
+app.patch('/todos/:id', controller.patchTodosId);
+app.listen(port, () => console.log(`App started on port ${port}`));
 
 module.exports = {app};
